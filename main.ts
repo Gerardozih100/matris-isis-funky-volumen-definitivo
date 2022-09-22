88442ea62ff4f1588f185d5d85a9f143aa4727d4
Gerardozih100 <106816077+Gerardozih100@users.noreply.github.com>
2022-09-21 23:04:43 -0500

    Se elimino la música Xque eran demasiados Byte

--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    music.setVolume(255)
+    for (let index = 0; index < 1; index++) {
+        music.playTone(330, music.beat(BeatFraction.Half))
+    }
+    for (let index = 0; index < 2; index++) {
+        max7219_matrix.scrollText(
+        "Agua Calentandose",
+        0,
+        0
+        )
+    }
+    for (let index = 0; index < 1; index++) {
+        music.playTone(494, music.beat(BeatFraction.Half))
+    }
+    max7219_matrix.scrollText(
+    "Fria",
+    0,
+    500
+    )
 })
-input.onButtonPressed(Button.B, function () {
-    music.setVolume(0)
-})
+let Inicio = 0
 max7219_matrix.setup(
 4,
 DigitalPin.P12,
@@ -18,50 +33,9 @@ true
 let Musica = 0
 let Texto = 0
 basic.forever(function () {
-    max7219_matrix.scrollText(
-    "#Te Amo Isis#",
-    0,
-    500
-    )
-})
-basic.forever(function () {
+    Inicio = 1
     if (input.buttonIsPressed(Button.A)) {
-        for (let index = 0; index < 10; index++) {
-            music.playTone(262, music.beat(BeatFraction.Half))
-            music.playTone(262, music.beat(BeatFraction.Half))
-            music.playTone(233, music.beat(BeatFraction.Half))
-            music.playTone(262, music.beat(BeatFraction.Half))
-            music.rest(music.beat(BeatFraction.Half))
-            music.playTone(196, music.beat(BeatFraction.Half))
-            music.rest(music.beat(BeatFraction.Half))
-            music.playTone(196, music.beat(BeatFraction.Half))
-            music.playTone(262, music.beat(BeatFraction.Half))
-            music.playTone(349, music.beat(BeatFraction.Half))
-            music.playTone(330, music.beat(BeatFraction.Half))
-            music.playTone(262, music.beat(BeatFraction.Half))
-            music.rest(music.beat(BeatFraction.Half))
-            Musica += Musica + 1
-        }
-    } else {
     	
-    }
-})
-basic.forever(function () {
-    if (Musica <= 3) {
-        music.playTone(262, music.beat(BeatFraction.Half))
-        music.playTone(262, music.beat(BeatFraction.Half))
-        music.playTone(233, music.beat(BeatFraction.Half))
-        music.playTone(262, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Half))
-        music.playTone(196, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Half))
-        music.playTone(196, music.beat(BeatFraction.Half))
-        music.playTone(262, music.beat(BeatFraction.Half))
-        music.playTone(349, music.beat(BeatFraction.Half))
-        music.playTone(330, music.beat(BeatFraction.Half))
-        music.playTone(262, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Half))
-        Musica += Musica + 1
     } else {
     	
     }
